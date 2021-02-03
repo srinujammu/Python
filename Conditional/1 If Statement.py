308a6ffa1aa931c2f53255c18d80e6f25de0b53a
srinujammu <67185283+srinujammu@users.noreply.github.com>
2021-02-03 14:52:34 +0530

    Add files via upload

---
+++ Conditional/1 If Statement.py
@@ -0,0 +1,8 @@
+x = 1
+
+if x == 0:
+    print('x is Zero')
+elif x == 1:
+    print('x is One')
+else:
+    print('Neither True')
---
+++ Conditional/2 Statement.py
@@ -0,0 +1,5 @@
+hungry = False
+
+x = 'Feed the bear now !' if hungry else 'Do not feed the bear'
+
+print(x)
